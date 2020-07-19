db57b960ad970bbf6cfa44a855f75e747959d6e5
Ben <ben.mitchell@medecision.com>
2020-07-18 23:10:26 -0400

    starting new theme

--- script/banner.js
+++ script/banner.js
@@ -1,4 +1,4 @@
-var imageIndex = -1;
+var imageIndex = 1;
 var headerImages = ["img/slideshow/slide-01.jpg", "img/slideshow/slide-02.jpg","img/slideshow/slide-03.jpg","img/slideshow/slide-04.jpg","img/slideshow/slide-05.jpg"];
 
 function rotateBackground()
@@ -7,7 +7,7 @@ function rotateBackground()
         if(imageIndex>=headerImages.length){
                 imageIndex=0;
         }
-        document.getElementById("header").style.backgroundImage  = "url('" + headerImages[imageIndex] + "')";
+        document.getElementById("rotating-images").src = headerImages[imageIndex];
 }
 
 window.setInterval(function(){rotateBackground();}, 10000);
