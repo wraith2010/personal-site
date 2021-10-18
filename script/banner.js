db8927fb2d3e9ee4a9bbb9035dde25698c9bc9bb
wraith2010 <benmitchelliii@gmail.com>
2021-10-18 00:06:37 -0400

    new look coming together

--- script/banner.js
+++ script/banner.js
@@ -1,14 +1,14 @@
 var imageIndex = -1;
 var images = ["img/slideshow/slide-01.jpg",
-                "img/slideshow/slide-02.jpg",
-                "img/slideshow/slide-03.jpg",
-                "img/slideshow/slide-04.jpg",
-                "img/slideshow/slide-05.jpg"];
+        "img/slideshow/slide-02.jpg",
+        "img/slideshow/slide-03.jpg",
+        "img/slideshow/slide-04.jpg",
+        "img/slideshow/slide-05.jpg"];
 var captions = ["Me standing infront of \"Big Ben\" in london. The joke writes its self.",
-               "Long horns outside our cabin in Virgina",
-               "Teaching light painting to our Youth Group",
-               "Riding the Tom Sawyer in Saint louis while visiting for the total eclipse.",
-               "Enjoying a drink in the Piazza San Marco in Venice"]
+        "Long horns outside our cabin in Virgina",
+        "Teaching light painting to our Youth Group",
+        "Riding the Tom Sawyer in Saint louis while visiting for the total eclipse.",
+        "Enjoying a drink in the Piazza San Marco in Venice"]
 
 
 function advanceBackground() {
@@ -22,14 +22,13 @@ function advanceBackground() {
 function rewindBackground() {
         imageIndex += -1;
         if (imageIndex < 0) {
-                imageIndex = images.length-1;
+                imageIndex = images.length - 1;
         }
         sethtml();
 }
 
-function sethtml(){
-        
-        document.getElementById("rotating-images").style.background =  'linear-gradient(rgba(255, 0, 0, 0.45),rgba(255, 0, 0, 0.45)), url(' + images[imageIndex] + ')';
+function sethtml() {
+        document.getElementById("rotating-images").style.background = 'linear-gradient(rgba(0, 0, 0, 0.40),rgba(0, 0, 0, 0.40)), url(' + images[imageIndex] + ')';
         document.getElementById("caption").innerText = captions[imageIndex];
 }
 
