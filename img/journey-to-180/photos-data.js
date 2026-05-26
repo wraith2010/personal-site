3febb2a087dc150bbe44eccebccdaa1eb7b382cf
Ben Mitchell <benmitchelliii@gmail.com>
2026-05-26 11:36:53 -0400

    improve journey to 180 page

---
+++ img/journey-to-180/photos-data.js
@@ -0,0 +1,9 @@
+var JOURNEY_PHOTOS = [
+  { "date": "Sep 15, 2018", "weight": 400, "note": "Way too big in London. This is where it starts.", "file": "IMG_20180915_181955.jpg" },
+  { "date": "Mar 15, 2020", "weight": 400, "note": "Pretty close to day one. Still had a long way to go.", "file": "emergency-stream-04.jpg" },
+  { "date": "Jan 19, 2026", "weight": 335, "note": "Getting exercise with Bandit even in the snow.", "file": "PXL_20260119_185518090.jpg" },
+  { "date": "Feb 6, 2026",  "weight": 328, "note": "Starting to see it in the mirror. Down 72 lb.", "file": "PXL_20260206_205841147.jpg" },
+  { "date": "Apr 7, 2026",  "weight": 324, "note": "Feeling trim at 324. Chasing Bandit in a Dog Park.", "file": "PXL_20260407_192639694.jpg" },
+  { "date": "May 2, 2026",  "weight": 319, "note": "Ran my first 5K in years.", "file": "PXL_20260502_143847156.TS-000.jpg" },
+  { "date": "May 8, 2026",  "weight": 315, "note": "The face doesn't lie. Down 85 lb.", "file": "PXL_20260508_182128536.jpg" }
+];
